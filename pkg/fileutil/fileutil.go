9db32e9b64e084e89d4a1df05ab2631af7a94fac
Ryan Talbot <ryan-talbot@outlook.com>
2022-06-01 11:19:55 +0100

    Added unzip func

--- pkg/fileutil/fileutil.go
+++ pkg/fileutil/fileutil.go
@@ -1,9 +1,13 @@
 package fileutil
 
 import (
+	"archive/zip"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func DownloadFile(filepath string, url string) error {
@@ -21,4 +25,56 @@ func DownloadFile(filepath string, url string) error {
 
 	_, err = io.Copy(out, response.Body)
 	return err
+}
+
+func Unzip(source string, destination string) ([]string, error) {
+	var filenames []string
+
+	reader, err := zip.OpenReader(source)
+	if err != nil {
+		return filenames, err
+	}
+	defer reader.Close()
+
+	for _, file := range reader.File {
+		filePath := filepath.Join(destination, file.Name)
+
+		if !strings.HasPrefix(filePath, filepath.Clean(destination)+string(os.PathSeparator)) {
+			return filenames, fmt.Errorf("%s: Invalid file path", filePath)
+		}
+
+		filenames = append(filenames, filePath)
+
+		if file.FileInfo().IsDir() {
+			err := os.MkdirAll(filePath, os.ModePerm)
+			if err != nil {
+				return nil, err
+			}
+			continue
+		}
+
+		if err = os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
+			return filenames, err
+		}
+
+		outFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
+		if err != nil {
+			return filenames, err
+		}
+
+		readerCloser, err := file.Open()
+		if err != nil {
+			return filenames, err
+		}
+
+		_, err = io.Copy(outFile, readerCloser)
+
+		outFile.Close()
+		readerCloser.Close()
+
+		if err != nil {
+			return filenames, err
+		}
+	}
+	return filenames, nil
 }
