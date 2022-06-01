192912e0342d7d4ba188e93ff855ec05004d9c7f
Ryan Talbot <ryan-talbot@outlook.com>
2022-06-01 11:01:58 +0100

    Added download file func

--- pkg/fileutil/fileutil.go
+++ pkg/fileutil/fileutil.go
@@ -1,1 +1,24 @@
 package fileutil
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func DownloadFile(filepath string, url string) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, response.Body)
+	return err
+}
