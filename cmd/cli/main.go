4cc52b8f9805386eb8a434517123edd050bc372d
Ryan Talbot <ryan-talbot@outlook.com>
2022-06-08 18:18:45 +0100

    Extracted main func bloat

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,59 +1,23 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
 	"twin/internal/github"
-	"twin/internal/utils"
 	"twin/pkg/fileutil"
 	"twin/pkg/sysutil"
 )
 
 func main() {
-	var destinationPath string
-	var branchName string
-
 	gitInstalled := sysutil.IsGitInstalled()
 
 	if gitInstalled == false {
 		fmt.Println("Git not found")
 	}
 
-	branchNamePointer := flag.String("branch", "", "The name of the branch you want")
-	outPointer := flag.String("out", "", "Destination path of the project")
-	noInitPointer := flag.Bool("no-init", false, "If set, will not auto init")
-	flag.Parse()
-
-	tail := flag.Args()
-
-	source, _ := utils.SplitGitHubURL(tail)
-	repoURL := source["repoURL"]
-	userName := source["userName"]
-	repoName := source["repoName"]
-
-	if *outPointer == "" {
-		destinationPath = source["repoName"]
-	} else {
-		destinationPath = *outPointer
-	}
-
-	if *branchNamePointer == "" {
-		mainBranchName, err := github.GetMainBranchName(userName, repoName)
-		if err != nil {
-			panic(err)
-		}
-		branchName = mainBranchName
-	} else {
-		existsBranch := github.VerifyBranchName(userName, repoName, *branchNamePointer)
-		if !existsBranch {
-			fmt.Printf("The branch %v not exists for repo %v\n", *branchNamePointer, repoURL)
-			os.Exit(1)
-		}
-		branchName = *branchNamePointer
-	}
+	prefs, init, _ := Flags()
 
 	tempDir := ".twin-temp"
 	err := os.Mkdir(".twin-temp", 0755)
@@ -61,7 +25,7 @@ func main() {
 		panic(err)
 	}
 
-	fileURL := "https://github.com/" + userName + "/" + repoName + "/archive/refs/heads/" + branchName + ".zip"
+	fileURL := "https://github.com/" + prefs["userName"] + "/" + prefs["repoName"] + "/archive/refs/heads/" + prefs["branchName"] + ".zip"
 	zipString := []string{tempDir, "zip.zip"}
 	zipFilePath := strings.Join(zipString, "/")
 	fileutil.DownloadFile(zipFilePath, fileURL)
@@ -78,14 +42,14 @@ func main() {
 
 	repoDirString := []string{unzipPath, repoDir.Name()}
 	repoDirPath := strings.Join(repoDirString, "/")
-	err = os.Rename(repoDirPath, "./"+destinationPath)
+	err = os.Rename(repoDirPath, "./"+prefs["destinationPath"])
 	if err != nil {
 		panic(err)
 	}
 
 	defer os.RemoveAll(tempDir)
 
-	if gitInstalled && *noInitPointer == false {
-		github.InitializeRepository(destinationPath)
+	if gitInstalled && *init == false {
+		github.InitializeRepository(prefs["destinationPath"])
 	}
 }
